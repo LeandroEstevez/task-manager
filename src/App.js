b7564a928d7823324b56cde3ec6f0cd61e7758f4
Leandro <leoest.dev@gmail.com>
2022-10-16 13:12:54 -0500

    maij app components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+
+    if (tasks.length === 0) {
+      setTasks([newTask]);
+    } else {
+      setTasks([...tasks, newTask]);
+    }
+  };
+
+  // Delet task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Task Manager"></Header>
+      {showAddTask && <AddTask onAdd={addTask}></AddTask>}
+      {tasks.length > 0 ? (
+        <Tasks
+          onDelete={deleteTask}
+          onToggle={toggleReminder}
+          tasks={tasks}
+        ></Tasks>
+      ) : (
+        "No Tasks to show"
+      )}
     </div>
   );
 }
